53706d6e9fc87fe3695d17c88c8cff7351a668b3
bruce <brucec@hotmail.com>
2021-10-15 17:03:01 -0700

    add queue impementation copied from internet

--- Debug/blink.d
+++ Debug/blink.d
@@ -5,6 +5,9 @@ blink.obj: D:/ccs/ccs_base/msp430/include/msp430fr5739.h
 blink.obj: D:/ccs/ccs_base/msp430/include/in430.h
 blink.obj: D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
 blink.obj: D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+blink.obj: D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stddef.h
+blink.obj: D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h
+blink.obj: D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h
 
 ../blink.c:
 
@@ -15,4 +18,10 @@ D:/ccs/ccs_base/msp430/include/in430.h:
 D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
 
 D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stddef.h:
+
+D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/_ti_config.h:
+
+D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/linkage.h:
 
