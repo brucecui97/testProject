75455c9d98ae9ce1417dc46d0c02baedf186aefc
bruce <brucec@hotmail.com>
2021-10-20 16:12:56 -0700

    add LED code

--- blink.c
+++ blink.c
@@ -50,6 +50,64 @@
 #define UNKNOWN_ACC 4
 
 
+#define LED1 BIT0
+#define LED2 BIT1
+#define LED3 BIT2
+#define LED4 BIT3
+#define LED5 BIT4
+#define LED6 BIT5
+#define LED7 BIT6
+#define LED8 BIT7
+
+
+//array of led pins
+unsigned char leds[]={LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8};
+
+void LEDInit(void)
+{
+    //first four bits
+    PJDIR |= LED1 + LED2 + LED3 + LED4;
+    //Leds Off
+    PJOUT &= ~(LED1 + LED2 + LED3 + LED4);
+    //last four leds
+    P3DIR |= LED5 + LED6 + LED7 + LED8;
+    //Leds Off
+    P3OUT &= ~(LED5 + LED6 + LED7 + LED8);
+}
+
+void LEDOn(unsigned char LEDn)
+{
+    if ((LEDn > 0) && (LEDn <= 4))
+    {
+        PJOUT |= leds[--LEDn];
+    }
+    if ((LEDn > 4) && (LEDn <= 8))
+    {
+        P3OUT |= leds[--LEDn];
+    }
+}
+void LEDOff(unsigned char LEDn)
+{
+    if ((LEDn > 0) && (LEDn <= 4))
+    {
+        PJOUT &= ~leds[--LEDn];
+    }
+    if ((LEDn > 4) && (LEDn <= 8))
+    {
+        P3OUT &= ~leds[--LEDn];
+    }
+}
+void LEDToggle(unsigned char LEDn)
+{
+    if ((LEDn > 0) && (LEDn <= 4))
+    {
+        PJOUT ^= leds[--LEDn];
+    }
+    if ((LEDn > 4) && (LEDn <= 8))
+    {
+        P3OUT ^= leds[--LEDn];
+    }
+}
 
 volatile unsigned int x_acc = 0;
 volatile unsigned int y_acc = 0;
@@ -90,6 +148,7 @@ void TakeADCMeas(void)
 volatile unsigned int ADCResult = 0;
 int main(void){
     WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+    LEDInit();
     // Configure clocks and timer
     CSCTL0 = 0xA500;                        // Write password to modify CS registers
     CSCTL1 = DCOFSEL0 + DCOFSEL1;           // DCO = 8 MHz
@@ -198,6 +257,7 @@ __interrupt void Timer_B (void)
       currState = UNKNOWN_ACC;
 
   }
+  LEDToggle(5);
 }
 
 
