32b7452f9a195a56c0e460d3a4210d2958fee7cb
bruce <brucec@hotmail.com>
2021-11-08 16:28:32 -0800

    add final firmware code EX5 423 Lab 3

--- Debug/blink.d
+++ Debug/blink.d
@@ -1,12 +1,15 @@
 # FIXED
 
 blink.obj: ../blink.c
+blink.obj: D:/ccs/ccs_base/msp430/include/msp430.h
 blink.obj: D:/ccs/ccs_base/msp430/include/msp430fr5739.h
 blink.obj: D:/ccs/ccs_base/msp430/include/in430.h
 blink.obj: D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
 blink.obj: D:/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
 
 ../blink.c:
+
+D:/ccs/ccs_base/msp430/include/msp430.h:
 
 D:/ccs/ccs_base/msp430/include/msp430fr5739.h:
 
--- blink.c
+++ blink.c
@@ -1,206 +1,278 @@
-//***************************************************************************************
-//  MSP430 Blink the LED Demo - Software Toggle P1.0
-//
-//  Description; Toggle P1.0 by xor'ing P1.0 inside of a software loop.
-//  ACLK = n/a, MCLK = SMCLK = default DCO
-//
-//                MSP430x5xx
-//             -----------------
-//         /|\|              XIN|-
-//          | |                 |
-//          --|RST          XOUT|-
-//            |                 |
-//            |             P1.0|-->LED
-//
-//  Texas Instruments, Inc
-//  July 2013
-//***************************************************************************************
+#include <msp430.h>
 
-#include "msp430fr5739.h"
-// The FRAM section from 0xD000 - 0xF000 is used by all modes
-// for performing writes to FRAM
-// Do not use this section for code or data placement.
-// It will get overwritten!
-#define ADC_START_ADD 0xD400
-#define ADC_END_ADD 0xF000
-#define FRAM_TEST_START 0xD400
-#define FRAM_TEST_END 0xF000
-#define MEM_UNIT 0x200
+#define ARRAY_SIZE 16
+#define K 300 // proportional control
 
-// Pin Definitions
-#define ACC_PWR_PIN       BIT7
-#define ACC_PWR_PORT_DIR  P2DIR
-#define ACC_PWR_PORT_OUT  P2OUT
-#define ACC_PORT_DIR      P3DIR
-#define ACC_PORT_OUT      P3OUT
-#define ACC_PORT_SEL0     P3SEL0
-#define ACC_PORT_SEL1     P3SEL1
-#define ACC_X_PIN         BIT0
-#define ACC_Y_PIN         BIT1
-#define ACC_Z_PIN         BIT2
+// variables for sending encoder data through UART
+volatile unsigned int interrupt_counter = 0;
+volatile unsigned int encoderA;
+volatile unsigned int encoderB;
+volatile unsigned int encoderA_prev = 0;
+volatile unsigned int encoderB_prev = 0;
 
-// Accelerometer Input Channel Definitions
-#define ACC_X_CHANNEL     ADC10INCH_12
-#define ACC_Y_CHANNEL     ADC10INCH_13
-#define ACC_Z_CHANNEL     ADC10INCH_14
+// variables for position control
+volatile signed int current_position;
+volatile signed int desired_position = 0;
+volatile unsigned int encoderA_overflow = 0;
+volatile unsigned int encoderB_overflow = 0;
 
-#define X_ACC 1
-#define Y_ACC 2
-#define Z_ACC 3
-#define UNKNOWN_ACC 4
+// variables for circular buffer
+volatile unsigned int start_index;
+volatile unsigned int end_index;
+volatile unsigned int count;
+unsigned char circ_buffer[ARRAY_SIZE];
 
+signed int get_position(void){
+    signed int position_temp;
+    position_temp = circ_buffer[start_index + 1];
+    position_temp = position_temp << 8;
+    position_temp |= circ_buffer[start_index + 2];
 
-
-volatile unsigned int x_acc = 0;
-volatile unsigned int y_acc = 0;
-volatile unsigned int z_acc = 0;
-volatile unsigned int currState = UNKNOWN_ACC;
-void SetupAccel(void)
-{
-  //Setup  accelerometer
-  // ~20KHz sampling
-  //Configure GPIO
-  ACC_PORT_SEL0 |= ACC_X_PIN + ACC_Y_PIN + ACC_Z_PIN;    //Enable A/D channel inputs
-  ACC_PORT_SEL1 |= ACC_X_PIN + ACC_Y_PIN + ACC_Z_PIN;
-  ACC_PORT_DIR &= ~(ACC_X_PIN + ACC_Y_PIN + ACC_Z_PIN);
-  ACC_PWR_PORT_DIR |= ACC_PWR_PIN;              //Enable ACC_POWER
-  ACC_PWR_PORT_OUT |= ACC_PWR_PIN;
-
-  // Allow the accelerometer to settle before sampling any data
-  __delay_cycles(2000);
-
-  //Single channel, once,
-  ADC10CTL0 &= ~ADC10ENC;                        // Ensure ENC is clear
-  ADC10CTL0 = ADC10ON + ADC10SHT_5;
-  ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10CONSEQ_0 + ADC10SSEL_0;
-  ADC10CTL2 = ADC10RES;
-  ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_12;
-  ADC10IV = 0x00;                          // Clear all ADC12 channel int flags
-  ADC10IE |= ADC10IE0;
+    return position_temp;
 }
 
-void TakeADCMeas(void)
-{
-  while (ADC10CTL1 & BUSY);
-  ADC10CTL0 |= ADC10ENC | ADC10SC ;       // Start conversion
-  __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-  __no_operation();                       // For debug only
+/*
+void set_direction(unsigned char direction){
+    switch (direction){
+    case 1: // CW
+        P3OUT |= BIT7;
+        P3OUT &= ~BIT6;
+        break;
+    case 2: // CCW
+        P3OUT |= BIT6;
+        P3OUT &= ~BIT7;
+        break;
+    }
+}
+*/
+
+void send_error(const char *str){
+
+    while (*str != '\0') {
+        /* Wait for the transmit buffer to be ready */
+        while((UCA0IFG & UCTXIFG)==0);
+
+        /* Transmit data */
+        UCA0TXBUF = *str;
+
+        str++;
+    }
+
 }
 
-volatile unsigned int ADCResult = 0;
-int main(void){
-    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-    // Configure clocks and timer
-    CSCTL0 = 0xA500;                        // Write password to modify CS registers
-    CSCTL1 = DCOFSEL0 + DCOFSEL1;           // DCO = 8 MHz
-    CSCTL2 = SELM0 + SELM1 + SELA0 + SELA1 + SELS0 + SELS1; // MCLK = DCO, ACLK = DCO, SMCLK = DCO
-
-    int pwmPeriod = 12400;
-    TB1CCR0 = pwmPeriod;                         // PWM Period
-
-    TB1CCTL1 = OUTMOD_7 + CCIE;                      // CCR1 reset/set
-    TB1CCR1 = pwmPeriod/2;                            // CCR1 PWM duty cycle
-    TB1CTL = TBSSEL_2 + MC_1 + TBCLR;         // SMCLK, up mode, clear TAR
-    P1DIR |= BIT0;
-    P1OUT |= BIT0;
-
-    // Configure ports for UCA0
-    P2SEL0 &= ~(BIT0 + BIT1);
-    P2SEL1 |= BIT0 + BIT1;
-
-    // Configure UCA0
-    UCA0CTLW0 = UCSSEL0;
-    UCA0BRW = 52;
-    UCA0MCTLW = 0x4900 + UCOS16 + UCBRF0;
-    UCA0IE |= UCRXIE;
-
-    SetupAccel();
-
-
-    while(1){
-        //loop through x y z axis to get the values and store them in global variable
-        //setup new ADC on different port
-
-        ADC10CTL0 &= ~ADC10ENC;                        // Ensure ENC is clear
-        ADC10CTL0 = ADC10ON + ADC10SHT_5;
-        ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10CONSEQ_0 + ADC10SSEL_0;
-        ADC10CTL2 = ADC10RES;
-        ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_12;
-        ADC10IV = 0x00;                          // Clear all ADC12 channel int flags
-        ADC10IE |= ADC10IE0;
-        TakeADCMeas();
-        x_acc = ADCResult>>2;
-        __delay_cycles(9000);
-
-        ADC10CTL0 &= ~ADC10ENC;                        // Ensure ENC is clear
-        ADC10CTL0 = ADC10ON + ADC10SHT_5;
-        ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10CONSEQ_0 + ADC10SSEL_0;
-        ADC10CTL2 = ADC10RES;
-        ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_13;
-        ADC10IV = 0x00;                          // Clear all ADC12 channel int flags
-        ADC10IE |= ADC10IE0;
-        TakeADCMeas();
-        y_acc = ADCResult>>2;
-        __delay_cycles(9000);
-
-        ADC10CTL0 &= ~ADC10ENC;                        // Ensure ENC is clear
-        ADC10CTL0 = ADC10ON + ADC10SHT_5;
-        ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10CONSEQ_0 + ADC10SSEL_0;
-        ADC10CTL2 = ADC10RES;
-        ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_14;
-        ADC10IV = 0x00;                          // Clear all ADC12 channel int flags
-        ADC10IE |= ADC10IE0;
-        TakeADCMeas();
-        z_acc = ADCResult>>2;
-        __delay_cycles(9000);
+void remove_from_buffer(void){
+    if (count == 0){
+        send_error("No elements in buffer");
+    }
+    else{
+//        while((UCA0IFG & UCTXIFG)==0);
+//        UCA0TXBUF = circ_buffer[start_index];
+        if (start_index == ARRAY_SIZE -1)
+            start_index = 0;
+        else
+            start_index++;
+        count--;
     }
 }
 
-#pragma vector=ADC10_VECTOR
-__interrupt void ADC10_ISR(void)
-{
-  switch(__even_in_range(ADC10IV,ADC10IV_ADC10IFG))
-  {
-    case ADC10IV_NONE: break;               // No interrupt
-    case ADC10IV_ADC10OVIFG: break;         // conversion result overflow
-    case ADC10IV_ADC10TOVIFG: break;        // conversion time overflow
-    case ADC10IV_ADC10HIIFG: break;         // ADC10HI
-    case ADC10IV_ADC10LOIFG: break;         // ADC10LO
-    case ADC10IV_ADC10INIFG: break;         // ADC10IN
-    case ADC10IV_ADC10IFG:
-             ADCResult = ADC10MEM0;
-             P1OUT ^= BIT0;
-             __bic_SR_register_on_exit(CPUOFF);
-             break;                          // Clear CPUOFF bit from 0(SR)
-    default: break;
-  }
+void add_to_buffer(unsigned char byte_data){
+    if (count == ARRAY_SIZE){
+        remove_from_buffer();
+        send_error("Buffer is full");
+    }
+    else{
+        circ_buffer[end_index] = byte_data;
+        if (end_index == ARRAY_SIZE - 1)
+            end_index = 0;
+        else
+            end_index++;
+        count++;
+    }
 }
 
-#pragma vector = TIMER1_B1_VECTOR
-__interrupt void Timer_B (void)
+/**
+ * main.c
+ */
+int main(void)
 {
-  TB1CCTL1 &= ~CCIFG;
-  if (currState == UNKNOWN_ACC){
-      UCA0TXBUF = 255;
-      currState = X_ACC;
-  }
-  else if (currState == X_ACC){
-      UCA0TXBUF = x_acc;
-      currState = Y_ACC;
-  }
-  else if (currState == Y_ACC){
-      UCA0TXBUF = y_acc;
-      currState = Z_ACC;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-  }
-  else if (currState == Z_ACC){
-      while ((UCA0IFG & UCTXIFG)==0);
-      UCA0TXBUF = z_acc;
-      while ((UCA0IFG & UCTXIFG)==0);
-      UCA0TXBUF = z_acc-3;
-      currState = UNKNOWN_ACC;
+    // circular buffer
+    start_index = 0;
+    end_index = 0;
+    count = 0;
 
-  }
+    // Configuring the clock
+    CSCTL0_H = 0xA5; // clock register password
+    CSCTL1 = DCORSEL + DCOFSEL0 + DCOFSEL1; // DCO at 24 MHz
+    CSCTL2 = SELA_3 + SELS_3 + SELM_3; // ACLK = MCLK = SMCLK = DCO
+
+    // Configuring pins for Encoder input to Timer A (P1.1, P1.2)
+    P1DIR &= ~(BIT1 + BIT2);
+    P1SEL0 &= ~(BIT1 + BIT2);
+    P1SEL1 |= BIT1 + BIT2;
+
+    // Configuring Timer A for Encoder input
+    TA0CTL = TASSEL_0 + MC_2 + TAIE; // Select TA0CLK and continuous mode
+    TA1CTL = TASSEL_0 + MC_2 + TAIE; // Select TA1CLK and continuous mode
+
+    // TB2.2 pin for DC Motor PWM output (P2.2)
+    P2DIR |= BIT2;
+    P2SEL0 |= BIT2;
+    P2SEL1 &= ~BIT2;
+
+    // DC Motor direction pins (P3.6, P3.7)
+    P3DIR |= BIT6 + BIT7;
+    P3OUT |= BIT7;
+    P3OUT &= ~BIT6; // set initially to CW
+
+    // Configuring Timer TB2.2
+    TB2CTL = TBSSEL_1 + MC_2; // select ACLK and continuous mode
+    TB2CCR0 = 0xFFFF;
+    TB2CCR2 = 0x0000; // set initially to 0 duty cycle
+    TB2CCTL2 = OUTMOD_7; // reset/set mode
+
+    // Configuring Timer TB2.1 for periodic interrupts
+    TB2CCTL1 = CCIE;
+    TB2CCR1 = 60000;
+
+    // Configure UART
+    UCA0CTLW0 |= UCSSEL_1; // ACLK
+    UCA0BRW = 156; // baud rate 9600 with DCO = 24 MHz
+    UCA0MCTLW = UCBRF_4 + UCOS16;
+
+    UCA0CTLW0 &= ~UCSWRST; // reset the bit to enable communication
+    UCA0IE = UCRXIE; // Interrupt enable for receive
+
+    // Set up ports for UCA0 (P2.0, P2.1)
+    P2SEL0 &= ~(BIT0 + BIT1);
+    P2SEL1 |= BIT0 + BIT1;
+
+    __enable_interrupt();
+
+    volatile unsigned long duty_cycle;
+
+    while(1){
+        if (current_position == desired_position){
+            TB2CCR2 = 0;
+        }
+        else if (current_position > desired_position){
+            duty_cycle = (current_position - desired_position)*K;
+            if (duty_cycle >= 0xFFFF) // cannot have duty cycle more than 100%
+                duty_cycle = 0xFFFE;
+//            else if (duty_cycle < 8000) // motor does not turn below this duty cycle
+//                duty_cycle = 8000;
+            TB2CCR2 = duty_cycle;
+            P3OUT |= BIT7;
+            P3OUT &= ~BIT6; // CW
+        }
+        else{
+            duty_cycle = (desired_position - current_position)*K;
+            if (duty_cycle >= 0xFFFF) // cannot have duty cycle more than 100%
+                duty_cycle = 0xFFFE;
+//            else if (duty_cycle < 8000) // motor does not turn below this duty cycle
+//                duty_cycle = 8000;
+            TB2CCR2 = duty_cycle;
+            P3OUT |= BIT6;
+            P3OUT &= ~BIT7; // CCW
+        }
+
+        if (count >= 3){
+            if (circ_buffer[start_index] == 255){
+                desired_position = get_position();
+            }
+
+            while(count != 0){
+                remove_from_buffer();
+            }
+        }
+    }
+
+    //return 0;
 }
 
+#pragma vector = USCI_A0_VECTOR
+__interrupt void RECEIVE_ISR(void){
+    unsigned char RxByte;
+    RxByte = UCA0RXBUF;
 
+    // expecting 3 byte data package: start byte, signed MSB of position, signed LSB of position
+    add_to_buffer(RxByte);
+}
+
+#pragma vector = TIMER2_B1_VECTOR // interrupt happening every 2.5 ms
+__interrupt void TRANSMIT_ISR(void){
+    TB2CCR1 += 60000;
+    TB2CCTL1 &= ~CCIFG;
+
+    interrupt_counter++;
+
+    if (interrupt_counter == 16){ // sending through UART every 40 ms
+        volatile unsigned int temp_byte;
+        volatile unsigned int temp_encoderA;
+        volatile unsigned int temp_encoderB;
+
+        encoderA = TA1R;
+        encoderB = TA0R;
+
+        current_position = (encoderA + encoderA_overflow*0x10000) - (encoderB + encoderB_overflow*0x10000);
+
+        if (encoderA_prev > encoderA)
+            temp_encoderA = 0x10000 - encoderA_prev + encoderA;
+        else
+            temp_encoderA = encoderA - encoderA_prev;
+
+        if (encoderB_prev > encoderB)
+            temp_encoderB = 0x10000 - encoderB_prev + encoderB;
+        else
+            temp_encoderB = encoderB - encoderB_prev;
+
+        while((UCA0IFG & UCTXIFG)==0);
+        UCA0TXBUF = 255;
+
+        while((UCA0IFG & UCTXIFG)==0);
+        temp_byte = temp_encoderA >> 8;
+        UCA0TXBUF = temp_byte;
+
+        while((UCA0IFG & UCTXIFG)==0);
+        UCA0TXBUF = temp_encoderA;
+
+        while((UCA0IFG & UCTXIFG)==0);
+        temp_byte = temp_encoderB >> 8;
+        UCA0TXBUF = temp_byte;
+
+        while((UCA0IFG & UCTXIFG)==0);
+        UCA0TXBUF = temp_encoderB;
+
+        interrupt_counter = 0;
+        encoderA_prev = encoderA;
+        encoderB_prev = encoderB;
+    }
+
+}
+
+/* if encoder B (TA0R) has already overflowed, then reset encoderB_overflow
+ * else set encoderA_overflow to 1
+ */
+#pragma vector = TIMER1_A1_VECTOR
+__interrupt void ENCODERA_ISR(void){
+    TA1CTL &= ~TAIFG;
+//    if (encoderB_overflow == 1)
+//        encoderB_overflow = 0;
+//    else
+//        encoderA_overflow = 1;
+    encoderA_overflow++;
+}
+
+/* if encoder A (TA1R) has already overflowed, then reset encoderA_overflow
+ * else set encoderB_overflow to 1
+ */
+#pragma vector = TIMER0_A1_VECTOR
+__interrupt void ENCODERB_ISR(void){
+    TA0CTL &= ~TAIFG;
+//    if (encoderA_overflow == 1)
+//        encoderA_overflow = 0;
+//    else
+//        encoderB_overflow = 1;
+    encoderB_overflow++;
+}
